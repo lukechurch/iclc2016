cb823d6a7d55f5b50494008951f0fa4eca3f5f0e
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2016-09-29 01:33:27 +0100

    Add implementation to ui.dart and a web project for testing it.

--- vis/lib/api.dart
+++ vis/lib/api.dart
@@ -7,12 +7,13 @@ import 'ui.dart';
 int addLine(
   int startX, int startY,
   int endX, int endY,
+  int width,
   int r, int g, int b, int a) {
 
   int id = allocateId();
   assert (!idExists(id));
 
-  var newLine = new Line(startX, startY, endX, endY, r, g, b, a);
+  var newLine = new Line(startX, startY, endX, endY, width, r, g, b, a);
   lines[id] = newLine;
 
   refreshDisplay();
@@ -52,9 +53,50 @@ bool setColour(int id,
     return true;
 }
 
+/// Set the coordinates of a line handle
+bool setLineCoordinates(int id,
+  int startX, int startY,
+  int endX, int endY) {
+
+    if (!idExists(id)) return false;
+
+    var obj = getObject(id);
+    if (obj is! Line) return false;
+
+    obj.startX = startX;
+    obj.startY = startY;
+    obj.endX = endX;
+    obj.endY = endY;
+
+    refreshDisplay();
+
+    return true;
+}
+
+/// Set the coordinates of a circle handle
+bool setCircleCoordinates(int id, int x, int y) {
+
+    if (!idExists(id)) return false;
+
+    var obj = getObject(id);
+    if (obj is! Circle) return false;
+
+    obj.x = x;
+    obj.y = y;
+
+    refreshDisplay();
+
+    return true;
+}
+
+
 /// Detele the element associated with a handle
 bool delete(int id) {
   if (!idExists(id)) return false;
 
-  return deleteObject(id);
+  bool deleted = deleteObject(id);
+
+  refreshDisplay();
+
+  return deleted;
 }
