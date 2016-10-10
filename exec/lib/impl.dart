e5920588fa2f053f2f261f1e4d30c40d44828778
Luke Church <luke@church.name>
2016-10-10 07:37:46 -0400

    Add first cut of multi-arg dispatcher

--- exec/lib/impl.dart
+++ exec/lib/impl.dart
@@ -1,8 +1,24 @@
+import 'instrinsics.dart' as intrinsics;
+
 const _TRACE_DEBUG = true;
 
 class Executive {
 
   final symbolTable = <String, dynamic>{};
+  final functionTable = <String, dynamic>{};
+
+  Executive()  {
+    functionTable.addAll({
+      "intrinsics.add" : intrinsics.add,
+      "intrinsics.sub" : intrinsics.sub,
+      "intrinsics.mul" : intrinsics.mul,
+      "intrinsics.div" : intrinsics.div,
+      "intrinsics.mod" : intrinsics.mod,
+      "intrinsics.not" : intrinsics.not,
+      "intrinsics.rpc_proxy" : intrinsics.rpc_proxy
+      }
+    );
+  }
 
   program(Map p) {
     for (var s in p["statements"]) {
@@ -22,8 +38,6 @@ class Executive {
       default:
         throw "unknown statement kind: $s";
     }
-
-
   }
 
   dynamic variableDeclaration(Map d) {
@@ -39,7 +53,40 @@ class Executive {
 
   dynamic call(Map c) {
     _trace("calling: $c");
-    return true;
+
+    String functionName = c["receiver"]["variable"];
+    List args = c["receiver"]["args"];
+
+    // Patch this up into an RPC call if it's not an intrinsic
+    if (!functionTable.containsKey(functionName))  {
+      args.insert(0, functionName);
+      functionName = "intrinsics.rpc_proxy";
+    }
+
+    return _callWithArgs(functionName, args);
+  }
+
+  dynamic _callWithArgs(String name, List a) {
+    if (a.length > 9) throw "Only 9 args currently supported";
+    switch (a.length) {
+      case 0: return functionTable[name]();
+      case 1: return functionTable[name](a[0]);
+      case 2: return functionTable[name](a[0], a[1]);
+      case 3: return functionTable[name](a[0], a[1], a[2]);
+      case 4: return functionTable[name](a[0], a[1], a[2], a[3]);
+      case 5: return functionTable[name](
+        a[0], a[1], a[2], a[3], a[4]);
+      case 6: return functionTable[name](
+        a[0], a[1], a[2], a[3], a[4], a[5]);
+      case 7: return functionTable[name](
+        a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+      case 8: return functionTable[name](
+        a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
+      case 9: return functionTable[name](
+        a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
+      default:
+        throw "Only 9 args supported. This shouldn't have got this far";
+    }
   }
 
   dynamic value(Map v) {
--- exec/lib/instrinsics.dart
+++ exec/lib/instrinsics.dart
@@ -2,3 +2,9 @@ sub(x, y) => x - y;
 add(x, y) => x + y;
 div(x, y) => x / y;
 mod(x, y) => x % y;
+mul(x, y) => x * y;
+not(x) => !x;
+
+rpc_proxy(name, arguments) {
+  
+}
