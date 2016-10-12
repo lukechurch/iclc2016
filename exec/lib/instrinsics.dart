5d40dde6d0637cefe738af5814c665c325a40ff2
Luke Church <luke@church.name>
2016-10-12 07:14:45 -0400

    Add RPC invocation

--- exec/lib/instrinsics.dart
+++ exec/lib/instrinsics.dart
@@ -1,3 +1,9 @@
+import 'dart:async';
+import 'package:json_rpc_2/json_rpc_2.dart' as json_rpc;
+import 'package:web_socket_channel/io.dart';
+import 'package:iclc_config/config.dart' as config;
+
+
 sub(x, y) => x - y;
 add(x, y) => x + y;
 div(x, y) => x / y;
@@ -10,6 +16,18 @@ lteq(x, y) => x <= y;
 gteq(x, y) => x >= y;
 eq(x, y) => x == y;
 
-rpc_proxy(name, arguments) {
-  print ("rpc_proxy eval: $name ($arguments)");
+json_rpc.Client client;
+
+rpc_proxy(name, arguments) async {
+  print ("Invoking rpc_proxy: $name ($arguments)");
+
+  if (client == null) {
+    Uri serverURL = Uri.parse('ws://localhost:${config.API_SERVER_PORT}');
+    var socket = new IOWebSocketChannel.connect(serverURL);
+    client = new json_rpc.Client(socket)
+      ..listen();
+  }
+
+  var resultJson = await client.sendRequest(name, arguments);
+  return resultJson['result'];
 }
